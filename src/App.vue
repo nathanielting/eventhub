b1a7efa482129a13a3b53c7d8378dd18e37803e9
Nate Ting <tingnate@gmail.com>
2021-12-07 22:03:10 -0500

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{ modelTitle }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    drawer:null
+  }),
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  props: ['title'],
+
+  created() {
+    console.log(this.title)
+  },
+  computed: {
+    modelName() {
+      if ('model' in this.$route.params){
+        return this.$route.params.model
+      }
+      return null
+    },
+    modelTitle() {
+      if ('model' in this.$route.params){
+        return `${this.$route.params.model} Viewer`
+      }
+      return this.title
+    }
+  },
+
+  methods: {
+    capitalize(word){
+        return word.charAt(0).toUpperCase()+word.slice(1)
+    },
+  }
+};
+</script>
